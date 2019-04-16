687cd250f0f034e8821ea035f6b5b5ba0f9588b7
Mike Johnson <MikeJ@utahcounty.gov>
2019-04-16 11:25:44 -0600

    change about/help titles to lower case

--- src/app.js
+++ src/app.js
@@ -33,14 +33,14 @@ app.get('',(req,res) => {
 
 app.get('/about', (req, res) => { 
   res.render('about', {
-    title: 'About',
+    title: 'about',
     name: 'Michael'
   })
 })
 
 app.get('/help', (req, res) => {
   res.render('help', {
-    title: 'Help',
+    title: 'help',
     name: 'Michael',
     message: 'This is the Help Message.'
   })
