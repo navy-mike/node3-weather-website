c5eb392993d8e1c19097c13f56896d532a0f3521
Mike Johnson <MikeJ@utahcounty.gov>
2019-04-16 12:10:59 -0600

    changed about/help titles

--- src/app.js
+++ src/app.js
@@ -33,14 +33,14 @@ app.get('',(req,res) => {
 
 app.get('/about', (req, res) => { 
   res.render('about', {
-    title: 'about',
+    title: 'About',
     name: 'Michael'
   }) 
 })
 
 app.get('/help', (req, res) => {
   res.render('help', {
-    title: 'help',
+    title: 'Help',
     name: 'Michael',
     message: 'This is the Help Message.'
   })
